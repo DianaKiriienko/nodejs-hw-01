0246d0730859e2d5d46855ec5b37ddbdafa6bc12
DianaKiriienko <diana.kirienko.homiak@gmail.com>
2024-05-27 23:40:31 +0300

    do tasks

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    try {
+        const contacts = [];
+
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    } catch (err) {
+    console.error(err);
+  }
+};
 
 await removeAllContacts();
